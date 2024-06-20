630b5710be10261ec7dc3438197403b7af78c94b
denny-erikson <denny.erikson@gmail.com>
2024-06-19 23:24:02 -0300

    fisrt config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import {  View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import {
+  requestForegroundPermissionsAsync, 
+  getCurrentPositionAsync, 
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy
+} from 'expo-location'
+import {styles} from './styles'
+import { useEffect, useRef, useState } from 'react';
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject | null>(null)
+  const mapRef = useRef<MapView>(null)
+
+
+  async function requestLocationPermissionsAsync(){
+    const { granted } = await requestForegroundPermissionsAsync()
+    if(granted){
+      const currentPosition = await getCurrentPositionAsync()
+      setLocation(currentPosition)
+    }
+  }
+
+  useEffect(()=>{
+    requestLocationPermissionsAsync()
+  }, [])
+
+  useEffect(()=>{
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1
+    }, (response)=>{
+      setLocation(response)
+      mapRef.current?.animateCamera({
+        pitch: 30,
+        center: response.coords
+      })
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location && 
+      <MapView 
+        ref={mapRef}
+        style={styles.map}
+        initialRegion={{
+          latitude:location.coords.latitude,
+          longitude:location.coords.longitude,
+          latitudeDelta:0.005,
+          longitudeDelta:0.005,
+        }}
+      >
+        <Marker 
+          coordinate={{
+            latitude:location.coords.latitude,
+            longitude:location.coords.longitude,
+          }}
+        />  
+      </MapView>}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
