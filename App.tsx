053b95d90c3c488bd653d51015d9ed75e870869c
denny-erikson <denny.erikson@gmail.com>
2024-06-24 09:11:10 -0300

    copy files from web

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
-import {  View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import {  Text, TouchableHighlight, View } from 'react-native';
+import MapView, { Callout, Marker,  } from 'react-native-maps';
 import {
   requestForegroundPermissionsAsync, 
   getCurrentPositionAsync, 
@@ -10,10 +10,16 @@ import {
 } from 'expo-location'
 import {styles} from './styles'
 import { useEffect, useRef, useState } from 'react';
+import { PointEnumType, PointType, pointEnum } from './src/commons/pointEnum';
+import {Point} from './src/components/Point'
+import styled from 'styled-components/native';
+import RoverImage  from './src/assets/images/rover-image.png'
+import IconNavigation from './src/assets/icons/icon-navigation.svg'
 
 export default function App() {
   const [location, setLocation] = useState<LocationObject | null>(null)
   const mapRef = useRef<MapView>(null)
+  const points = Object.values(pointEnum);
 
 
   async function requestLocationPermissionsAsync(){
@@ -54,6 +60,7 @@ export default function App() {
           latitudeDelta:0.005,
           longitudeDelta:0.005,
         }}
+        mapType='satellite'
       >
         <Marker 
           coordinate={{
@@ -61,9 +68,105 @@ export default function App() {
             longitude:location.coords.longitude,
           }}
         />  
+
+        {
+          points.map((point: PointType)=> { 
+            const [lat, long] = point.location
+            return (
+              <Marker 
+                key={point.name} 
+                coordinate={{
+                  latitude:lat,
+                  longitude:long,
+                }}
+                >
+              {point.slug !== "camping" && <Callout>
+                  {/* <Point 
+                    type='marker'
+                    name={point.name}
+                    category={point.category}
+                    imageUrl={point.imageUrl}
+                    location={[lat, long]}
+                  /> */}
+
+                  <CardPointer>
+                    <ImagePointer source={RoverImage || {uri: point.imageUrl}} />
+                    <ContentPointer>
+                      <TitlePoint>{point.name}</TitlePoint>
+                      <TitlePoint>{point.category}</TitlePoint>
+                      <ButtonPointer>
+                          <IconButtonPointer source={IconNavigation}/>
+                          <TouchableHighlight onPress={()=>{}}>
+                            <TextButtonPointer>Ver mais</TextButtonPointer>
+                          </TouchableHighlight>
+                      </ButtonPointer>
+                    </ContentPointer>
+                    
+                  </CardPointer>
+                </Callout>}
+              </Marker>
+          )})
+        }
       </MapView>}
     </View>
   );
 }
+
+
+export const CardPointer = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 4px;
+
+  gap: 12px;
+`
+
+export const ImagePointer = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 46px;
+`
+
+export const TitlePoint = styled.Text`
+  font-weight: bold;
+  font-size:14px;
+  color: #1B1D54;
+`
+
+export const ContentPointer = styled.View`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+`
+
+export const ButtonPointer = styled.View`      
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 8px 6px;
+`
+
+export const TextButtonPointer = styled.Text`
+  color: #2E3190;
+  font-weight: bold;   
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 120px;
+`
+
+export const IconButtonPointer = styled.Image`
+  margin-top: -5px;
+  transform: rotate(27deg);
+  height:16px;
+  width: 16px;
+  margin-right: 4px;
+`
+
+
 
 
